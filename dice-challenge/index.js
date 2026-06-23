2c79191a6fd3994b595c9da7796d7425d752b436
David Campina <campina@newsaktuell.de>
2026-06-23 17:20:42 +0200

    Link random number with output for corresponding dice image and text message.

--- dice-challenge/index.js
+++ dice-challenge/index.js
@@ -1,4 +1,21 @@
 function diceGenerator(){
   let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-  return randomNumber1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let dice1 = "images/dice"+ randomNumber1 +".png";
+let dice2 = "images/dice"+ randomNumber2 +".png";
+ 
+document.querySelector(".img1").setAttribute("src",dice1);
+document.querySelector(".img2").setAttribute("src",dice2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerText="Player 1 wins!";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerText="Player 2 wins!";
+} else {
+    document.querySelector("h1").innerText="Draw!";
 }
+
+}
+
+diceGenerator(); 
